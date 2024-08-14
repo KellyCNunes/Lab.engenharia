a4592c122111fcd6aa486c483343314f88035fc1
KellyCNunes <kelly.crisnunes@hotmail.com>
2024-08-13 21:56:23 -0300

    Update exerc2.js

--- atividade1/exerc2.js
+++ atividade1/exerc2.js
@@ -1,14 +1,18 @@
 let numero = prompt("Digite um número inteiro primo:");
-
-
+let contador = 0;
 // Verifica se o número é válido
 if (!isNaN(numero) && numero > 0) {
     // Verifica se o número é primo
-    if (numero % numero === 0) {
+    for (i=1; i< numero; i++){
+        if (numero % i === 0) {
+            contador++;      
+        }
+    } if (contador >= 2){
         alert("O número " + numero + " é primo.");
-    } else {
-        alert("O número " + numero + " não é primo.");
     }
-} else {
+    else{ alert("O número " + numero + " não é primo.")}
+}
+    
+ else {
     alert("Por favor, digite um número inteiro positivo válido.");
 }
