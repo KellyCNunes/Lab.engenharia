b81bf4e926322223aa7f571a28c688c52d674691
KellyCNunes <138325492+KellyCNunes@users.noreply.github.com>
2024-08-19 18:54:08 -0300

    Atualizar o exerc4.js

--- atividade1/exerc4.js
+++ atividade1/exerc4.js
@@ -1,17 +1,15 @@
 function verificarDado() {
-    // Solicita a entrada do usuário
+    
     let dado = prompt("Por favor, insira um dado:");
 
-    // Pergunta se o usuário deseja verificar o tipo do dado
+   
     let confirmar = confirm("Deseja verificar o tipo do dado informado?");
 
-    // Verifica se o usuário deseja continuar
     if (confirmar) {
-        // Detecta o tipo de dado e escreve na página
+        // Detecta o tipo de dado 
         let tipoDado = typeof dado;
         document.body.innerHTML = "<h1>O tipo do dado informado é: " + tipoDado + "</h1>";
     } else {
-        // Mensagem caso o usuário não deseje verificar
         document.body.innerHTML = "<h1>Obrigado por visitar esta página.</h1>";
     }
 }
