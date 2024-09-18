ed5709706fd5183fa4221bd0bd6dab260af16562
KellyCNunes <138325492+KellyCNunes@users.noreply.github.com>
2024-09-17 22:09:49 -0300

    17/09

---
+++ atividade3/exerc1.js
@@ -0,0 +1,29 @@
+var items = [];
+
+function updateList() {
+    items.sort();
+
+    const list = document.getElementById('itemList');
+    list.innerHTML = '';
+
+    items.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = item;
+        list.appendChild(li);
+    });
+}
+
+document.getElementById('itemForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const itemInput = document.getElementById('itemInput');
+    const newItem = itemInput.value.trim();
+
+    if (newItem) {
+        items.push(newItem);
+        
+        updateList();
+        itemInput.value = '';
+    }
+    return false;
+});
